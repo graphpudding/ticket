8945b70935d364b17693dad97f9debd58a4675ac
motya <pblhelka@gmail.com>
2019-05-23 01:06:52 +0300

    Initial commit
    commit

---
+++ js/custom.js
@@ -0,0 +1,21 @@
+var content=document.getElementById('js-content');
+var navi=document.getElementById('js-navi');
+var but=document.getElementsByClassName('js-but');
+var phone=document.getElementById('js-phone');
+var cart=document.getElementById('js-cart');
+var logo=document.getElementById('js-logo');
+var buy=document.getElementById('js-buy');
+var buttons=document.getElementById('js-buttons');
+
+for (var i = 0; i < but.length; i++) {
+	but[i].addEventListener('click',function () {
+	content.classList.remove('content-box--display');
+	navi.style.top=0+"px";
+	navi.classList.add('b-head_navi-background');
+	navi.classList.remove('b-head_navi-buttons');
+	phone.style.display="inline-block";
+	cart.style.display="inline-block";
+	logo.style.display="none";
+	buy.style.display="none";
+}) 
+}
